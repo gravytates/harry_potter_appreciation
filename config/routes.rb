9565834dd4ba6a7e805d1266541c1e849740d51f
Asia Kane and Grady Shelton <asia@>
2017-06-28 09:10:30 -0700

    add CRUD for characters

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root :to => 'characters#index'
+
   resources :characters
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
